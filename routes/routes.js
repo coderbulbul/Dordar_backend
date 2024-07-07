5177a10959003d5d44c4b9f513665434bbddd158
coderbulbul <coderbulbul@gmail.com>
2024-07-08 01:02:44 +0600

    test

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,11 @@ const paymentController = require("../controllers/paymentController");
 const middleware = require("../middleware/middleware");
 
 // Test Router
-router.post("/bkash/payment/test", paymentController.test);
+router.post(
+  "/bkash/payment/test",
+  middleware.bkash_auth,
+  paymentController.test
+);
 
 // Route Bkash payment create
 router.post(
